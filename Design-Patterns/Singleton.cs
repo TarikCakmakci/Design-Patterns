3e1e23ee26cba3c4e36772e4a1c06db1c58d49a0
tarikcakmakci <tarikcakmakci@outlook.com>
2023-01-04 00:52:28 -0800

    Update
    Singleton update

---
+++ Design-Patterns/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Design_Patterns
+{
+    public sealed class Singleton
+    {
+        public int size { get; set; }
+        public string name { get; set; }
+
+        private static readonly Lazy<Singleton> _instance = new Lazy<Singleton>(() => new Singleton());
+
+        //private static readonly object padlock = new object();
+        private Singleton() { }
+        public static Singleton Instance
+        {
+            get
+            {
+                return _instance.Value;
+            }
+        }
+       
+
+    }
+}
