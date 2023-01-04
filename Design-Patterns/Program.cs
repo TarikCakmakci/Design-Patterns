47011a6189fd0f88bcecd26a990203677c096cd6
tarikcakmakci <tarikcakmakci@outlook.com>
2023-01-04 05:01:21 -0800

    Update
    Object pool with stack

--- Design-Patterns/Program.cs
+++ Design-Patterns/Program.cs
@@ -10,10 +10,36 @@ namespace Design_Patterns
     {
         static void Main(string[] args)
         {
-            //test
-            Company c = Company.getInstance();
-            c.showMessage();
+            //SİNGLETON
+            //Company c = Company.getInstance();
+            //c.showMessage();
+            //Console.Read();
+
+            //OBJECT POOL TEST
+            var intPool = new objectPool<int>(
+            create: () => 0,       // Create a new integer with a value of 0
+            reset: i => i = 0      // Reset the integer by setting its value to 0
+            );
+
+            var number = intPool.GetObject();
+
+            // Use the integer
+            Console.WriteLine("Havuzdan alınan nesne: "+number);
+
+            // Return the integer to the pool
+            //havuza geri koyuldu
+            intPool.PutObject(number);
+            // Get a list of objects from the pool
+            var objects = intPool.GetObjects();
+
+            // Iterate through the objects
+            foreach (var obj in objects)
+            {
+                Console.WriteLine("Havuzdaki nesne:"+obj);
+            }
             Console.Read();
+           
+
 
         }
     }
